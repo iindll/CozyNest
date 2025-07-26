c08cfefb224fae5358edbfcf6b4d736445d8736c
iindll <nadaabobaker11@gmail.com>
2025-07-26 07:31:35 +0300

    modify login styles2

--- vite-project/src/components/Authentication/Login.jsx
+++ vite-project/src/components/Authentication/Login.jsx
@@ -1,9 +1,10 @@
 import "./Login.css"
 import { Link } from "react-router-dom";
-
+import imge from "../../assets/Login.jpg"
 const Login = () => {
   return (
     <div className='Login'>
+       <img src={imge} alt="" />
         <form >
             <div className="userInfo">
             <div className="text-section">
@@ -12,10 +13,13 @@ const Login = () => {
             <input type="password" placeholder="password" />
             <div className="Forgetlink">
                 <Link className="Forget" to="./forget-password">Forget Password?</Link>
+
             </div>
             <div className="button-Login">
                 <button>Login</button>
             </div>
+                <Link className="Register" to="./SignUp">You dont have account?</Link>
+
             </div>
         </form>
     </div>
